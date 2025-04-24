dc8e419368184811b0a95451b2311e3ab214971e
Emily Sammons <easammons8002@gmail.com>
2025-04-24 10:20:02 -0700

    initial code for name assignemnt

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        //Console.WriteLine("Hello Sandbox World!");
+
+        Console.WriteLine("Input your first name: ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine($"Your first name is: {firstName}");
+
+        Console.WriteLine("Input your last name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your full name is: {firstName} {lastName}");
+
+        int x = 10;
+        if (x == 10)
+        {
+            Console.WriteLine("x is 10");
+        }
     }
 }
