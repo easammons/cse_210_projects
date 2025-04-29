a01b0710a2a9b2cacaf33bce1f2e8297b186ee92
Emily Sammons <easammons8002@gmail.com>
2025-04-29 09:51:38 -0700

    tried out commiting (added a WritleLine statement)

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,20 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Console.WriteLine("Hello Sandbox World!");
+        // //Console.WriteLine("Hello Sandbox World!");
 
-        Console.WriteLine("Input your first name: ");
-        string firstName = Console.ReadLine();
-        Console.WriteLine($"Your first name is: {firstName}");
+        // Console.WriteLine("Input your first name: ");
+        // string firstName = Console.ReadLine();
+        // Console.WriteLine($"Your first name is: {firstName}");
 
-        Console.WriteLine("Input your last name: ");
-        string lastName = Console.ReadLine();
-        Console.WriteLine($"Your full name is: {firstName} {lastName}");
+        // Console.WriteLine("Input your last name: ");
+        // string lastName = Console.ReadLine();
+        // Console.WriteLine($"Your full name is: {firstName} {lastName}");
 
-        int x = 10;
-        if (x == 10)
-        {
-            Console.WriteLine("x is 10");
-        }
+        // int x = 10;
+        // if (x == 10)
+        // {
+        //     Console.WriteLine("x is 10");
+        // }
+
+        Console.WriteLine("Hey how are you? ");
     }
 }
