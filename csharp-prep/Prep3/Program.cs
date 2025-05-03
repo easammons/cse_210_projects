4168ac97034ecdaee4e09546491e56339ffd7998
Emily Sammons <easammons8002@gmail.com>
2025-05-03 13:16:46 -0700

    did prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("What is your magic number? ");
+        // string input1 = Console.ReadLine();
+        // int magic = int.Parse(input1);
+        Random random = new Random();
+
+        int magic = random.Next(100);
+        
+        int going = 1;
+
+        while(going == 1)
+        {
+
+            Console.WriteLine("What is your guess? ");
+            string input2 = Console.ReadLine();
+            int guess = int.Parse(input2);
+
+
+            if (guess == magic)
+            {
+                Console.WriteLine("You guessed it!");
+                break;
+            }
+            else if(guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if(guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number");
+            }
+        }
     }
 }
