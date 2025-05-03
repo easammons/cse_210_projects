cc34f329064ec823bccfb44a819faa559e53eb65
Emily Sammons <easammons8002@gmail.com>
2025-05-03 13:35:19 -0700

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+
+            if (num == 0)
+            {
+                break;
+            }
+
+            numbers.Add(num);
+        }
+
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"\nThe sum of the numbers is: {sum}");
+
+        double average = numbers.Average();
+        Console.WriteLine($"\nThe average of the numbers is: {average}");
+
+        int mini = numbers.Min();
+        int maxi = numbers.Max();
+
+        Console.WriteLine($"\nThe minimum is: {mini}");
+        Console.WriteLine($"\nThe maximum is: {maxi}");
+
+     }
 }
